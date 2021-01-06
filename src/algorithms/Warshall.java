94e4ab577258a81b4084afc23b4d793b7a21d070
Frnacisco <fruiz.rc@gmail.com>
2021-01-05 23:22:34 -0400

    Finish the Warshall algorithm implementation, also make a new class with static method for arrays or matrix

--- src/algorithms/Warshall.java
+++ src/algorithms/Warshall.java
@@ -29,7 +29,29 @@ public class Warshall {
             }
             marshall_matrix.add(aux_integers);
         }
+        System.out.println("Tabla de adyacencia\n");
         arrays_mehotds.showMatrix(marshall_matrix);
+
+        for(byte k=0; k < nodes_list.size(); k++){
+
+            for(byte i=0; i < marshall_matrix.size(); i++){
+
+                for(byte j=0; j < marshall_matrix.size(); j++){
+                    if(i == k || j == k){
+                        continue;
+                    }
+                    if(marshall_matrix.get(k).get(i) == 1 && marshall_matrix.get(j).get(k) == 1){
+                        Vector aux = marshall_matrix.get(j);
+                        aux.set(i, 1);
+                        marshall_matrix.set(j, aux);
+                    }
+                }
+            }
+            System.out.printf("Tabla del Nodo %s\n", k);
+            arrays_mehotds.showMatrix(marshall_matrix);
+
+
+        }
 
 
         return marshall_matrix;
