3175f72d20419dc3ce0be4ea5d4b571cc49276c8
Frnacisco <fruiz.rc@gmail.com>
2021-01-06 23:28:28 -0400

    changed typo marshall for warshall

--- src/algorithms/Warshall.java
+++ src/algorithms/Warshall.java
@@ -8,7 +8,7 @@ import java.util.Vector;
 public class Warshall {
 
     public static Vector<Vector<Vector<Integer>>> warshallAlgorithm(Graph graph) {
-        Vector<Vector<Integer>> marshallMatrix = new Vector<>(1, 1);
+        Vector<Vector<Integer>> warshallMatrix = new Vector<>(1, 1);
         Vector<Node> nodesList = graph.getNodeList();
         Vector<Vector<Vector<Integer>>> allTransitiveMatrix = new Vector<>(1,1);
 
@@ -24,26 +24,26 @@ public class Warshall {
                 }
 
             }
-            marshallMatrix.add(auxIntegers);
+            warshallMatrix.add(auxIntegers);
         }
-        allTransitiveMatrix.add(new Vector<>(marshallMatrix));
+        allTransitiveMatrix.add(new Vector<>(warshallMatrix));
 
         for(byte k=0; k < nodesList.size(); k++){
 
-            for(byte i=0; i < marshallMatrix.size(); i++){
+            for(byte i=0; i < warshallMatrix.size(); i++){
 
-                for(byte j=0; j < marshallMatrix.size(); j++){
+                for(byte j=0; j < warshallMatrix.size(); j++){
                     if(i == k || j == k){
                         continue;
                     }
-                    if(marshallMatrix.get(k).get(i) == 1 && marshallMatrix.get(j).get(k) == 1){
-                        Vector<Integer> aux = new Vector<>(marshallMatrix.get(j));
+                    if(warshallMatrix.get(k).get(i) == 1 && warshallMatrix.get(j).get(k) == 1){
+                        Vector<Integer> aux = new Vector<>(warshallMatrix.get(j));
                         aux.set(i, 1);
-                        marshallMatrix.set(j, aux);
+                        warshallMatrix.set(j, aux);
                     }
                 }
             }
-            allTransitiveMatrix.add(new Vector<>(marshallMatrix));
+            allTransitiveMatrix.add(new Vector<>(warshallMatrix));
 
 
         }
